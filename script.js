a658afd8b468a21b5784f57fcbf3b661959ac600
vishnuviratGit <vishnuvirat564@gmail.com>
2025-03-27 13:49:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ document.getElementById("MyForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent form submission
     volume_sphere();
 });
+
 function volume_sphere() {
-    //Write your code here
-		let radiusInput=document.getElementById("radius").value;
-	    let radius=parseFloat(radiusInput);
-	   if(isNan(radius) || radius<0){
-		   document.getElementById("volume").value="NaN";
-		   return;
-	   }
-	  let volume=(4/3)*(Math.PI)*(radius**3);
-	  document.getElementById("volume").value=volume.toFixed(4);
-	
-  
-} 
+    let radiusInput = document.getElementById("radius").value;
+    let radius = parseFloat(radiusInput);
+    
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById("volume").value = "NaN";
+        return;
+    }
+    
+    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    document.getElementById("volume").value = volume.toFixed(4);
+}
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
