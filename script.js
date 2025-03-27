afb463389e348ffe6351ad217043e092f7ff0307
vishnuviratGit <vishnuvirat564@gmail.com>
2025-03-27 13:46:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+document.getElementById("MyForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+    volume_sphere();
+});
 function volume_sphere() {
     //Write your code here
+		let radiusInput=document.getElementById("radius").value;
+	    let radius=parseFloat(radiusInput);
+	   if(isNan(radius) || radius<0){
+		   document.getElementById("volume").value="NaN";
+		   return;
+	   }
+	  let volume=(4/3)*(Math.PI)*(radius**3);
+	  document.getElementById("volume").value=volume.toFixed(4);
+	
   
 } 
 
